1901986530acaf31ba0ce4b1f45487b901b474c2
jantari <jantari@live.com>
2017-09-10 13:44:24 +0200

    added tux ascii artwork and output redirection support (no more Write-Host !)

--- powerfetch.ps1
+++ powerfetch.ps1
@@ -1,8 +1,9 @@
 #### Screenfetch for PowerShell
 #### by jantari ( https://github.com/jantari )
 #### Inspiration from Julian Chow ( https://github.com/JulianChow94 )
-#### ASCII artwork based on the one in WinScreeny by
+#### windows-flag ASCII artwork based on the one in WinScreeny by
 #### nijikokun ( https://github.com/nijikokun ) used with explicit permission
+#### tux ASCII artwork from http://ascii.co.uk/art/tux
 
 # with [Environment]::NewLine you can create a newline on Windows AND Linux
 # this is a little tip as a thank you to anyone reading this source code
@@ -81,81 +82,117 @@ $UsedRamPercent = ($UsedRam / $TotalRam) * 100;
 $UsedRamPercent = "{0:N0}" -f $UsedRamPercent;
 
 ## Array with ASCII art
-[string[]] $art = @'
-         [91m,zz::A33tz;,[0m                   
+if ($unix) {
+    [string[]]$art = @'
+          _nnnn_         
 '@, @'
-         [91m@t:::EE333EE3[0m [92m.[0m                
+         dGGGGMMb        
 '@, @'
-        [91m;Et:::EE33EEE7[0m [92m@Ee.,     .,g[0m    
+        @p~qp~~qMb       
 '@, @'
-       [91m.St:::EE333EE3[0m [92m;EEEEEEttt333#[0m    
+        M|O||O) M|       
 '@, @'
-       [91m@t:::zE333EE3`[0m[92m.SEEEEEtttt33Q[0m     
+        @,----.JM|       
 '@, @'
-      [91m:Et:::EE333EE7[0m [92m@EEEEEEtttt33F[0m     
+       JS^\__/  qKL      
 '@, @'
-      [91m@P*''``''*4Qj[0m [92m:EEEEEEtttt33@[0m      
+     dZP        qKRb     
 '@, @'
-     [94m,,::::33tz;,[0m [91m*[0m [92m@EEEEEEttz33Q7[0m      
+    dZP          qKKb    
 '@, @'
-    [94m;t::::ztttt33)[0m [93m.[0m [92m*4EEEjjjiP*[0m        
+   fZP            SMMb   
 '@, @'
-   [94m:tt::::ttttt33[0m [93m:Z3:..[0m  [92m``[0m [93m,,g[0m        
+   HZM            MMMM   
 '@, @'
-   [94mit::::ztttt33F[0m [93mAEEEEEtttttE3F[0m        
+   FqM            MMMM   
 '@, @'
-  [94m;t:::::tttt33V[0m [93m;EEEEEttttttt3[0m         
+ __| ".        |\dS"qML  
 '@, @'
-  [94mEt::::ztttt337[0m [93m@EEEEttttttt3F[0m          
+ |    `.       | `' \Zq  
 '@, @'
-  [94m@P*''``''*4Qj[0m [93m;EEEEEtttttttZ`[0m          
+_)      \.___.,|     .' 
 '@, @'
-             [94m`[0m  [93mEEEEEtttttttj7[0m            
+\____   )MMMMMP|   .'   
 '@, @'
-                [93m`^VEtjjjz>*`[0m          
+     `-'       `--' 
 '@
+} else {
+    [string[]] $art = @'
+        [91m,zz::A33tz;,[0m                   
+'@, @'
+        [91m@t:::EE333EE3[0m [92m.[0m                
+'@, @'
+       [91m;Et:::EE33EEE7[0m [92m@Ee.,     .,g[0m    
+'@, @'
+      [91m.St:::EE333EE3[0m [92m;EEEEEEttt333#[0m    
+'@, @'
+      [91m@t:::zE333EE3`[0m[92m.SEEEEEtttt33Q[0m     
+'@, @'
+     [91m:Et:::EE333EE7[0m [92m@EEEEEEtttt33F[0m     
+'@, @'
+     [91m@P*''``''*4Qj[0m [92m:EEEEEEtttt33@[0m      
+'@, @'
+    [94m,,::::33tz;,[0m [91m*[0m [92m@EEEEEEttz33Q7[0m      
+'@, @'
+   [94m;t::::ztttt33)[0m [93m.[0m [92m*4EEEjjjiP*[0m        
+'@, @'
+  [94m:tt::::ttttt33[0m [93m:Z3:..[0m  [92m``[0m [93m,,g[0m        
+'@, @'
+  [94mit::::ztttt33F[0m [93mAEEEEEtttttE3F[0m        
+'@, @'
+ [94m;t:::::tttt33V[0m [93m;EEEEEttttttt3[0m         
+'@, @'
+ [94mEt::::ztttt337[0m [93m@EEEEttttttt3F[0m          
+'@, @'
+ [94m@P*''``''*4Qj[0m [93m;EEEEEtttttttZ`[0m          
+'@, @'
+            [94m`[0m  [93mEEEEEtttttttj7[0m            
+'@, @'
+               [93m`^VEtjjjz>*`[0m          
+'@
+}
 
 ####### Printing Output #########
 
 # Line 1 - HostName
-Write-Host $art[0] "[91m$Username[0m@[91m$Machine[0m"
+Write-Output "$($art[0]) [91m$Username[0m@[91m$Machine[0m"
 
 # Line 2 - OS
-Write-Host $art[1] "[91mOS:[0m $OS $BitVer"
+Write-Output "$($art[1]) [91mOS:[0m $OS $BitVer"
 
 # Line 3 - Kernel
-Write-Host $art[2] "[91mKernel:[0m $Kernel"
+Write-Output "$($art[2]) [91mKernel:[0m $Kernel"
 
 # Line 4 - Uptime
-Write-Host $art[3] "[91mUptime:[0m ${uptimeHours}:${uptimeMinutes}"
+Write-Output "$($art[3]) [91mUptime:[0m ${uptimeHours}:${uptimeMinutes}"
 # .Days"d "$uptime.Hours"h " $uptime.Minutes"m " $uptime.Seconds"s " -Separator "";
 
 # Line 5 - Motherboard
-Write-Host $art[4] "[91mMotherboard:[0m" $Motherboard.Manufacturer $Motherboard.Product
+Write-Output "$($art[4]) [91mMotherboard:[0m $($Motherboard.Manufacturer) $($Motherboard.Product)"
 
 # Line 6 - Shell (Hardcoded since it is unlikely anybody can run this without powershell)
-Write-Host $art[5] "[91mShell:[0m PowerShell $($PSVersionTable.PSVersion)"
+Write-Output "$($art[5]) [91mShell:[0m PowerShell $($PSVersionTable.PSVersion)"
 
 # Line 7 - Cmdlets
-Write-Host $art[6] "[91mCmdlets:[0m $cmdlets"
+Write-Output "$($art[6]) [91mCmdlets:[0m $cmdlets"
 
 # Line 8 - Resolution (for primary monitor only)
-Write-Host $art[7] "[91mResolution:[0m$($display.CurrentHorizontalResolution) x$($display.CurrentVerticalResolution) @$($display.CurrentRefreshRate) Hz"
+Write-Output "$($art[7]) [91mResolution:[0m$($display.CurrentHorizontalResolution) x$($display.CurrentVerticalResolution) @$($display.CurrentRefreshRate) Hz"
 
 # Line 9 - CPU
-Write-Host $art[8] "[91mCPU:[0m $CPU"
+Write-Output "$($art[8]) [91mCPU:[0m $CPU"
 
 # Line 10 - GPU
-Write-Host $art[9] "[91mGPU:[0m $GPU"
+Write-Output "$($art[9]) [91mGPU:[0m $GPU"
 
 # Line 11 - Ram
-Write-Host $art[10] "[91mRAM:[0m $UsedRam MB / $TotalRam MB ([92m$UsedRamPercent%[0m)"
+Write-Output "$($art[10]) [91mRAM:[0m $UsedRam MB / $TotalRam MB ([92m$UsedRamPercent%[0m)"
 
 # Line 12 - Disk
-Write-Host $art[11] "[91mDisk:[0m $UsedDiskSizeGB GB / $DiskSizeGB GB ([92m$UsedDiskPercent%[0m)"
+Write-Output "$($art[11]) [91mDisk:[0m $UsedDiskSizeGB GB / $DiskSizeGB GB ([92m$UsedDiskPercent%[0m)"
 
 # Empty Lines
-Write-Host $art[12]
-Write-Host $art[13]
-Write-Host $art[14]
-Write-Host $art[15]
+Write-Output $art[12]
+Write-Output $art[13]
+Write-Output $art[14]
+Write-Output $art[15]
